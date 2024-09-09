f81f146039ad75e9d9b78ebeb1c1af903df69b19
Luisa Santo <luisasanto@microsoft.com>
2024-09-08 21:42:55 -0700

    Add gh-pages deployment configuration and prepare for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        TEMPLATE_ID : process.env.TEMPLATE_ID,
-        SERVICE_ID : process.env.SERVICE_ID,
-        PUBLIC_KEY : process.env.PUBLIC_KEY,
+    output: "export",
+    assetPrefix: "/",
+    basePath: "",
+    images: {
+      unoptimized: true,
     },
-}
+  };  
 
 module.exports = nextConfig
