ddabd672bd4347a4a794739cb13656d88673f829
Luisa Santo <luisasanto@microsoft.com>
2024-09-08 21:37:49 -0700

    Remove node_modules from tracking and add to .gitignore

--- node_modules/locomotive-scroll/node_modules/@studio-freight/lenis/dist/types/maths.d.ts
+++
@@ -1,4 +0,0 @@
-export function clamp(min: any, input: any, max: any): number;
-export function truncate(value: any, decimals?: number): number;
-export function lerp(start: any, end: any, amt: any): number;
-export function clampedModulo(dividend: any, divisor: any): number;
