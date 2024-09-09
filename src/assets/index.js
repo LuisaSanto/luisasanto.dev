924af212a721a96159d5fdd8bb256d0dfd358454
Luisa Santo <luisasanto@microsoft.com>
2024-09-08 21:51:17 -0700

    Add CNAME file for custom domain www.yourdomain.com

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,16 +10,16 @@ import bug from "./bug.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import git from "./tech/git.png";
-import peacockLogo from "./tech/peacockLogo.png";
-import ComcastLogo from "./tech/ComcastLogo.png";
-import SkyLogo from "./tech/SkyLogo.png";
-import teamsLogo from "./tech/teamsLogo.png";
-import groupMeLogo from "./tech/groupMeLogo.png";
-import talkdeskLogo from "./tech/talkdeskLogo.png";
-import cernLogo from "./tech/cernLogo.png";
-import swiftLogo from "./tech/swiftLogo.png"; 
-import github from "./tech/github.png";
+import git from "./git.png";
+import peacockLogo from "./peacockLogo.png";
+import ComcastLogo from "./ComcastLogo.png";
+import SkyLogo from "./SkyLogo.png";
+import teamsLogo from "./teamsLogo.png";
+import groupMeLogo from "./groupMeLogo.png";
+import talkdeskLogo from "./talkdeskLogo.png";
+import cernLogo from "./cernLogo.png";
+import swiftLogo from "./swiftLogo.png"; 
+import github from "./github.png";
 
 export {
   logo,
